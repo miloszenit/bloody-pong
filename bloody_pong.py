176517f694752397698df3d252f5276fe7db8852
miloszenit <xymilos@gmail.com>
2022-10-07 23:37:06 +0200

    Define and draw rectangles

--- bloody_pong.py
+++ bloody_pong.py
@@ -8,11 +8,24 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Welcome to BloodyPong!')
 
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+computer = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+player = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
+
+bg_color = '#1E1D1D'
+my_color = '#C32E2E'
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+
+	screen.fill(bg_color)
+	pygame.draw.rect(screen, my_color, computer)
+	pygame.draw.rect(screen, my_color, player)
+	pygame.draw.ellipse(screen, my_color, ball)
+	pygame.draw.aaline(screen, my_color, (screen_width / 2, 0),(screen_width / 2, screen_height))
 	
 	pygame.display.flip()
 	clock.tick(140)
