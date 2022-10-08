08d2613fd970280eb664e5b0460b7725b3e12caa
miloszenit <xymilos@gmail.com>
2022-10-08 12:09:39 +0200

    Add scores

--- bloody_pong.py
+++ bloody_pong.py
@@ -1,15 +1,23 @@
 import pygame, sys, random
 
 def ball_movement():
-	global ball_speed_x, ball_speed_y
+	global ball_speed_x, ball_speed_y, player_score, computer_player_score
 	
 	ball.x += ball_speed_x
 	ball.y += ball_speed_y
 
 	if ball.top <= 0 or ball.bottom >= screen_height:
 		ball_speed_y *= -1
-	if ball.left <= 0 or ball.right >= screen_width:
+
+	# Player score	
+	if ball.left <= 0: 
 		ball_reset()
+		player_score += 1
+
+	# Computer player score
+	if ball.right >= screen_width:
+		ball_reset()
+		computer_player_score += 1
 
 	if ball.colliderect(player) or ball.colliderect(computer_player):
 		ball_speed_x *= -1
@@ -64,6 +72,11 @@ ball_speed_y = 5 * random.choice((1,-1))
 player_speed = 0
 computer_player_speed = 5
 
+# Score
+player_score = 0
+computer_player_score = 0
+basic_font = pygame.font.Font('freesansbold.ttf', 32)
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -91,5 +104,11 @@ while True:
 	pygame.draw.ellipse(screen, my_color, ball)
 	pygame.draw.aaline(screen, my_color, (screen_width / 2, 0),(screen_width / 2, screen_height))
 	
+	player_text = basic_font.render(f'{player_score}', False, my_color)
+	screen.blit(player_text,(430, 30))
+
+	computer_player_text = basic_font.render(f'{computer_player_score}', False, my_color)
+	screen.blit(computer_player_text,(355, 30))
+
 	pygame.display.flip()
 	clock.tick(140)
