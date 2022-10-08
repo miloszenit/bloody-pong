2910cf3b939f805ce02f8cad010c451829a21017
miloszenit <xymilos@gmail.com>
2022-10-08 13:07:58 +0200

    Add a timer

--- bloody_pong.py
+++ bloody_pong.py
@@ -1,7 +1,7 @@
 import pygame, sys, random
 
 def ball_movement():
-	global ball_speed_x, ball_speed_y, player_score, computer_player_score
+	global ball_speed_x, ball_speed_y, player_score, computer_player_score, score_time
 	
 	ball.x += ball_speed_x
 	ball.y += ball_speed_y
@@ -11,12 +11,12 @@ def ball_movement():
 
 	# Player score	
 	if ball.left <= 0: 
-		ball_reset()
+		score_time = pygame.time.get_ticks()
 		player_score += 1
 
 	# Computer player score
 	if ball.right >= screen_width:
-		ball_reset()
+		score_time = pygame.time.get_ticks()
 		computer_player_score += 1
 
 	if ball.colliderect(player) or ball.colliderect(computer_player):
@@ -42,12 +42,33 @@ def computer_player_movement():
 		computer_player.bottom = screen_height
 
 def ball_reset():
-	global ball_speed_x, ball_speed_y
+	global ball_speed_x, ball_speed_y, score_time
+
+	current_time = pygame.time.get_ticks()
 
 	ball.center = (screen_width/2, screen_height/2)
 	ball_speed_y *= random.choice((1,-1))
 	ball_speed_x *= random.choice((1,-1))	
-		
+
+	if current_time - score_time < 600:
+		timer_three = basic_font.render("3", False, my_color)
+		screen.blit(timer_three,(screen_width/2 - 10, screen_height/2 + 20))
+
+	if 700 < current_time - score_time < 1200:
+		timer_two = basic_font.render("2", False, my_color)
+		screen.blit(timer_two,(screen_width/2 - 10, screen_height/2 + 20))
+
+	if 1400 < current_time - score_time < 1800:
+		timer_one = basic_font.render("1", False, my_color)
+		screen.blit(timer_one,(screen_width/2 - 10, screen_height/2 + 20))
+
+	if current_time - score_time < 1800:
+		ball_speed_y, ball_speed_x = 0,0
+	else:
+		ball_speed_x = 5 * random.choice((1,-1))
+		ball_speed_y = 5 * random.choice((1,-1))
+		score_time = None
+
 # BloddyPong general setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -76,6 +97,7 @@ computer_player_speed = 5
 player_score = 0
 computer_player_score = 0
 basic_font = pygame.font.Font('freesansbold.ttf', 32)
+score_time = True
 
 while True:
 	for event in pygame.event.get():
@@ -104,6 +126,9 @@ while True:
 	pygame.draw.ellipse(screen, my_color, ball)
 	pygame.draw.aaline(screen, my_color, (screen_width / 2, 0),(screen_width / 2, screen_height))
 	
+	if score_time:
+		ball_reset()
+
 	player_text = basic_font.render(f'{player_score}', False, my_color)
 	screen.blit(player_text,(430, 30))
 
